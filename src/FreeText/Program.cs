dbadea5b83807b11147bfc7426968347fdeaabb3
wk-j <somnuk.wk@gmail.com>
2018-11-02 17:35:15 +0700

    Rotate

--- src/FreeText/Program.cs
+++ src/FreeText/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
@@ -19,20 +20,36 @@ namespace FreeText {
                 var left = box.Left;
                 var top = box.Top;
 
-                var newRectangle = new iTextSharp.text.Rectangle(left + 20, top - 20, left + 250, top - 40);
+                // var newRectangle = new iTextSharp.text.Rectangle(left + 20, top - 20, left + 250, top - 40);
+                var newRectangle = new iTextSharp.text.Rectangle(left, top, left + 250, top - 40);
+                newRectangle.UseVariableBorders = false;
+
                 var pcb = new iTextSharp.text.pdf.PdfContentByte(stamper.Writer);
                 pcb.SetColorFill(iTextSharp.text.BaseColor.RED);
 
-                var annot = iTextSharp.text.pdf.PdfAnnotation.CreateFreeText(stamper.Writer, newRectangle, "Hello, world!", pcb);
+
+                var text = "Hello, world!";
+
+                var annot = iTextSharp.text.pdf.PdfAnnotation.CreateFreeText(stamper.Writer, newRectangle, text, pcb);
                 annot.Flags = iTextSharp.text.pdf.PdfAnnotation.FLAGS_PRINT;
 
-                annot.BorderStyle = new iTextSharp.text.pdf.PdfBorderDictionary(0, 0);
+                // annot.BorderStyle = new iTextSharp.text.pdf.PdfBorderDictionary(0, 0);
+                // annot.Color = GetBaseColor(Color.Yellow);
+                // annot.Put(PdfName.DS, new PdfString("font: " + "Tohama" + " " +
+                //           (10 * 0.75f) + "pt; text-align: left; color: " +
+                //           GetBaseColor(Color.Blue)));
+                // annot.Put(PdfName.RC, new PdfString(text.Replace("(?i)<br */?> ", "\n").Replace(" & nbsp; ", " ")));
+                // annot.Put(PdfName.ROTATE, new PdfNumber(0));
+
                 stamper.AddAnnotation(annot, 1);
                 stamper.Close();
 
                 var output = ms.ToArray();
                 System.IO.File.WriteAllBytes(".output/Hello.pdf", output);
             }
+        }
+        static BaseColor GetBaseColor(Color color) {
+            return new BaseColor(color.R, color.G, color.B);
         }
     }
 }
